0741e76713e56a93c5f8e8db824852d722ef6c31
ido navarro <navarro.ido@gmail.com>
2022-11-17 08:02:48 +0200

    Check child elements

--- code.ts
+++ code.ts
@@ -14,32 +14,48 @@ for(const node of figma.currentPage.selection)
 
   // resize the duplicated node
   duplicateFrame.resize(node.width,node.height);
-if(node.type == 'FRAME' || node.type == 'GROUP')
-{
-  for(const child of node.children)
-  {
-    var skelement;
-    skelement = figma.createRectangle();
-    skelement.resize(child.width,child.height);
-
-    skelement.x = child.x;
-    skelement.y = child.y;
-
-    skelement.cornerRadius = 6;
-    skelement.fills = [{
-      type: "SOLID",
-      color: {
-        b: 0.9725490212440491,
-        g: 0.9607843160629272,
-        r: 0.9529411792755127
-    }
-
-    }];
 
 
-    duplicateFrame.appendChild(skelement);
+  function skelify(node){
+    for(const child of node.children)
+    {
+      if (child.height > 0.01) {
+  
+            if((child.type !== 'FRAME' && (child.type !== 'GROUP') && (child.type !== 'INSTANCE')))
+            {
+              var skelement;
+              skelement = figma.createRectangle();
+              skelement.resize(child.width,child.height);
+  
+              skelement.x = child.x;
+              skelement.y = child.y;
+  
+              skelement.cornerRadius = 6;
+              skelement.fills = [{
+                type: "SOLID",
+                color: {
+                  b: 0.9725490212440491,
+                  g: 0.9607843160629272,
+                  r: 0.9529411792755127
+              }
+  
+              }];
+  
+  
+              duplicateFrame.appendChild(skelement);
+            }
+            else {
+              skelify(child);
+            }
+  
+  
+      }
+  
   }
-}
+  }
+
+   
+
   
 }
 figma.closePlugin();
